604154b6ec356811695ec3d4782e4e435e9bd455
polinabatanova <polab98@gmail.com>
2026-03-25 10:16:18 +0000

    TDD - Added test for road vs grass logic (failing)

--- test/view/MapTest.java
+++ test/view/MapTest.java
@@ -2,8 +2,7 @@ package view;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class MapTest {
@@ -27,6 +26,13 @@ public class MapTest {
     public void testMapHeight() {
         GameMap map = new GameMap();
         assertEquals(600, map.getPreferredSize().height, "Height should be 600px");
+    }
+
+    @Test
+    public void testIsRoadLogic() {
+        GameMap map = new GameMap();
+        assertTrue(map.isRoad(1, 0), "Row 1, Col 0 should be recognized as a road");
+        assertFalse(map.isRoad(0, 0), "Row 0, Col 0 should be recognized as grass");
     }
 
 }
