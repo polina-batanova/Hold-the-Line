ed391b7b7e907e80419f769a53213f119b05d34b
polinabatanova <polab98@gmail.com>
2026-03-25 08:58:32 +0000

    Created a 2D array grid to represent a map with two paths

--- src/view/GameMap.java
+++ src/view/GameMap.java
@@ -11,5 +11,16 @@ public class GameMap extends JPanel {
     public GameMap() {
         setPreferredSize(new Dimension(COLS * TILE_SIZE, ROWS * TILE_SIZE));
     }
+    private int[][] grid = {
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {1,1,1,1,1,0,0,0,0,0,0,0,0,0,2,2,2,2,2,0},
+            {0,0,0,0,1,0,0,0,0,0,0,0,0,0,2,0,0,0,2,0},
+            {0,0,0,0,1,1,1,1,1,1,1,1,1,1,2,0,0,0,2,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,2,2,2,2,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+    };
+    public int[][] getGrid() {
+        return grid;
+    }
 
 }
