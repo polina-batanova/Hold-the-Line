74ac8f4b141585aa912abd389b40683a13d18616
polinabatanova <polab98@gmail.com>
2026-03-25 08:56:43 +0000

    Created GameMap constants and preferred panel dimensions

--- src/view/GameMap.java
+++ src/view/GameMap.java
@@ -1,6 +1,15 @@
 package view;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class GameMap extends JPanel {
+    private final int TILE_SIZE = 40;
+    private final int ROWS = 15;
+    private final int COLS = 20;
+
+    public GameMap() {
+        setPreferredSize(new Dimension(COLS * TILE_SIZE, ROWS * TILE_SIZE));
+    }
+
 }
