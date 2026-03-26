afea9b32f1cd65056ff3a625af9c326acc4e4648
polinabatanova <polab98@gmail.com>
2026-03-26 08:04:52 +0000

    Linked PNG assets to sprite map keys

--- src/view/AssetLoader.java
+++ src/view/AssetLoader.java
@@ -8,7 +8,12 @@ public class AssetLoader {
     private Map<String, BufferedImage> sprites = new HashMap<>();
 
     public AssetLoader() {
-
+        loadSprites();
+    }
+    private void loadSprites() {
+        sprites.put("grass", loadImage("/assets/tiles/FieldTile_01.png"));
+        sprites.put("road", loadImage("/assets/tiles/FieldTile_05.png"));
+        sprites.put("tower_spot", loadImage("/assets/placeholders/PlaceForTower1.png"));
     }
 
     public BufferedImage getSprite(String key) {
