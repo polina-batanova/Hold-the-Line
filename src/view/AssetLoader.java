2821addd093c7baa2aeffc93bad9d70c687e9b1c
polinabatanova <polab98@gmail.com>
2026-03-26 08:01:50 +0000

    Created AssetLoader skeleton for TDD

---
+++ src/view/AssetLoader.java
@@ -0,0 +1,17 @@
+package view;
+
+import java.awt.image.BufferedImage;
+import java.util.HashMap;
+import java.util.Map;
+
+public class AssetLoader {
+    private Map<String, BufferedImage> sprites = new HashMap<>();
+
+    public AssetLoader() {
+
+    }
+
+    public BufferedImage getSprite(String key) {
+        return null;
+    }
+}
