39fdbe79d6c8aab8bfc2b8d55b1b19fd1104216b
polinabatanova <polab98@gmail.com>
2026-03-26 09:57:22 +0000

    Refined logic

--- src/view/GameMap.java
+++ src/view/GameMap.java
@@ -20,8 +20,8 @@ public class GameMap extends JPanel {
             {0,0,0,0,1,0,0,0,0,0,0,0,0,0,2,0,0,0,2,0},
             {0,0,0,0,1,1,1,1,1,1,1,1,1,1,2,0,0,0,2,0},
             {0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,2,2,2,2,0},
-            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,3,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,3,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
             {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
             {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
             {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
@@ -39,6 +39,7 @@ public class GameMap extends JPanel {
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
+
         for (int r = 0; r < ROWS; r++) {
             for (int c = 0; c < COLS; c++) {
                 BufferedImage tileImg;
@@ -49,13 +50,12 @@ public class GameMap extends JPanel {
                     tileImg = assetLoader.getSprite("grass");
                 }
 
-
                 if (tileImg != null) {
                     g.drawImage(tileImg, c * TILE_SIZE, r * TILE_SIZE, TILE_SIZE, TILE_SIZE, null);
                 }
 
 
-                if (grid[r][c] == 0) {
+                if (grid[r][c] == 3) {
                     BufferedImage spot = assetLoader.getSprite("tower_spot");
                     if (spot != null) {
                         g.drawImage(spot, c * TILE_SIZE, r * TILE_SIZE, TILE_SIZE, TILE_SIZE, null);
@@ -64,17 +64,12 @@ public class GameMap extends JPanel {
             }
         }
     }
+        public boolean isRoad ( int r, int c){
+            return isValidCoordinate(r, c) && grid[r][c] > 0 && grid[r][c] < 3;
 
-
-    public boolean isRoad(int r, int c) {
-        if (!isValidCoordinate(r, c)) {
-            return false;
+        }
+        public boolean isValidCoordinate ( int r, int c){
+            return r >= 0 && r < ROWS && c >= 0 && c < COLS;
         }
 
-        return grid[r][c] > 0;
     }
-    public boolean isValidCoordinate(int r, int c) {
-        return r >= 0 && r < ROWS && c >= 0 && c < COLS;
-    }
-
-}
