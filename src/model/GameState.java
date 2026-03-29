343a245bee63e48fd37731b4dc4047b47d0058f7
Davyd <levidskijdavid@gmail.com>
2026-03-29 15:04:06 +0100

    Implement Player, GameState and GameManager classes

--- src/model/GameState.java
+++ src/model/GameState.java
@@ -1,4 +1,9 @@
 package model;
 
-public class GameState {
+public enum GameState {
+    NOT_STARTED,
+    PLAYER1_TURN,
+    PLAYER2_TURN,
+    ROUND_EXECUTION,
+    GAME_OVER
 }
--- src/view/GameWindow.java
+++
@@ -1,7 +0,0 @@
-package view;
-
-import org.junit.jupiter.api.Test;
-
-public class GameWindow {
-    @Test
-}
