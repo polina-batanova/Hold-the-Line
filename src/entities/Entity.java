9b8358c8befd19e747c81527f975e9a72e484b07
Anton Zahrai <zagrajanton@gmail.com>
2026-03-30 02:29:26 +0100

    Add Entity abstract base class stub

--- src/entities/Entity.java
+++ src/entities/Entity.java
@@ -1,4 +1,30 @@
 package entities;
 
-public class Entity {
+// abstract base class for all entities
+// holds position (row, col) and name
+public abstract class Entity {
+
+    protected int row;
+    protected int col;
+    protected String name;
+
+    // constructs an entity at the given position with a name
+    public Entity(String name, int row, int col) {
+        // todo: implement validation and assignment
+    }
+
+    public String getName() {
+        // todo: implement
+        return null;
+    }
+
+    public int getRow() {
+        // todo: implement
+        return 0;
+    }
+
+    public int getCol() {
+        // todo: implement
+        return 0;
+    }
 }
