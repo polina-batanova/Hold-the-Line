1b332f487cdfe3627526bd920f5f9c9b1c8c333e
Anton Zahrai <zagrajanton@gmail.com>
2026-03-30 20:29:18 +0100

    Restore src/model from member1-gameManager

--- src/model/GameManager.java
+++ src/model/GameManager.java
@@ -1,4 +1,64 @@
 package model;
 
 public class GameManager {
+    private Player player1;
+    private Player player2;
+    private GameState state;
+    private int currentRound;
+
+    public GameManager(Player player1, Player player2) {
+        if (player1 == null || player2 == null) {
+            throw new IllegalArgumentException("Players cannot be null.");
+        }
+
+        this.player1 = player1;
+        this.player2 = player2;
+        this.state = GameState.NOT_STARTED;
+        this.currentRound = 0;
+    }
+
+    public void startGame() {
+        currentRound = 1;
+        state = GameState.PLAYER1_TURN;
+    }
+
+    public void nextTurn() {
+        switch (state) {
+            case PLAYER1_TURN:
+                state = GameState.PLAYER2_TURN;
+                break;
+            case PLAYER2_TURN:
+                state = GameState.ROUND_EXECUTION;
+                break;
+            case ROUND_EXECUTION:
+                currentRound++;
+                state = GameState.PLAYER1_TURN;
+                break;
+            default:
+                throw new IllegalStateException("Cannot move to next turn from state: " + state);
+        }
+    }
+
+    public void startRound() {
+        if (state != GameState.PLAYER2_TURN) {
+            throw new IllegalStateException("Round can only start after Player 2 turn.");
+        }
+        state = GameState.ROUND_EXECUTION;
+    }
+
+    public Player getPlayer1() {
+        return player1;
+    }
+
+    public Player getPlayer2() {
+        return player2;
+    }
+
+    public GameState getState() {
+        return state;
+    }
+
+    public int getCurrentRound() {
+        return currentRound;
+    }
 }
