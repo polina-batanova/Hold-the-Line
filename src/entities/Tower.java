d6c40367ce7d7895d55b5454d6376a8dce89a14e
Anton Zahrai <zagrajanton@gmail.com>
2026-03-30 03:00:37 +0100

    Add Tower class stub

--- src/entities/Tower.java
+++ src/entities/Tower.java
@@ -1,4 +1,37 @@
 package entities;
 
-public class Tower {
+// represents placed defensive structure
+// extends Entity for shared position and name
+public class Tower extends Entity {
+
+    private int range; // attack range
+    private int damage; // dmg per hit
+    private int cost; // cost to place
+
+    // constructs a tower with all stats
+    public Tower(String name, int row, int col, int range, int damage, int cost) {
+        super(name, row, col);
+        // todo: implement validation and assignment
+    }
+
+    public int getRange() {
+        // todo: implement
+        return 0;
+    }
+
+    public int getDamage() {
+        // todo: implement
+        return 0;
+    }
+
+    public int getCost() {
+        // todo: implement
+        return 0;
+    }
+
+    // checks if mob in a tower's attack range
+    public boolean isInRange(Mob mob) {
+        // todo: implement
+        return false;
+    }
 }
